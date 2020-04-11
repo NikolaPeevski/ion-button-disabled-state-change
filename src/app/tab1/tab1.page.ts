9056b1562643dd4ce280726aed2296f7a4a64cad
nikolapeevski <Jeratull@hotmail.com>
2020-04-11 06:38:58 -0500

    Init commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Tab1Page {
 
-  constructor() {}
+  public isDisabled = false;
+
+  constructor(public changeRef: ChangeDetectorRef) {}
+
+  changeState() {
+    this.isDisabled = !this.isDisabled;
+    // this.changeRef.detectChanges();
+  }
 
 }
